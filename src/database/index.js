3a0bc23380fbfcb46e4330c3c9896a87b70ea8a4
Samuel Benício <54580052+briariustecno@users.noreply.github.com>
2021-07-14 16:05:26 -0300

    Reconfigure the connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
 mongoose.connect('mongodb://localhost/noderest', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
