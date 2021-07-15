2b7c7d4f1580773a362941ab2061dca59c841797
Samuel Benício <54580052+briariustecno@users.noreply.github.com>
2021-07-15 10:43:17 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const string = 'mongodb://' + DB_HOST + ':' + DB_PORT + '/' + DB_NAME;
 
-mongoose.connect('mongodb://localhost/noderest', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(string, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
